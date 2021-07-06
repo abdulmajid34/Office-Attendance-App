62dccc6bb78dcbbce10b0f26607e8324fac27cd5
abdulmajid <abdulmajid08180881@gmail.com>
2021-07-07 04:05:03 +0700

    init db

---
+++ server/migrations/20210706081932-create-employee.js
@@ -0,0 +1,33 @@
+'use strict';
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    await queryInterface.createTable('Employees', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      nama: {
+        type: Sequelize.STRING
+      },
+      divisi: {
+        type: Sequelize.STRING
+      },
+      status: {
+        type: Sequelize.STRING
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    });
+  },
+  down: async (queryInterface, Sequelize) => {
+    await queryInterface.dropTable('Employees');
+  }
+};
